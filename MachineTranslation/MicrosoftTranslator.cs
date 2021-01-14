8882dccf7c31af56e1c34ee51ee9a7a5498f587a
NourEldin Yasser <noursalem95@gmail.com>
2021-01-14 13:39:23 +0200

    add translate api sample code

---
+++ MachineTranslation/MicrosoftTranslator.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MachineTranslation
+{
+    public class MicrosoftTranslator
+    {
+        private const string Host = "https://api.cognitive.microsofttranslator.com";
+        private const string Path = "/translate?api-version=3.0";
+        private const string UriParams = "&to=";
+
+        private static HttpClient _client = new HttpClient();
+
+        private readonly string _key;
+        private readonly string _region;
+
+
+        public MicrosoftTranslator(string region, string key)
+        {
+            _key = key ?? throw new ArgumentNullException(nameof(key));
+            _region = region ?? throw new ArgumentNullException(nameof(region));
+        }
+
+        public async Task<TranslatorResponse> TranslateAsync(string text, string targetLocale, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // From Cognitive Services translation documentation:
+            // https://docs.microsoft.com/en-us/azure/cognitive-services/translator/quickstart-csharp-translate
+            var body = new object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var request = new HttpRequestMessage())
+            {
+                var uri = Host + Path + UriParams + targetLocale;
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", _key);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", _region);
+
+                var response = await _client.SendAsync(request, cancellationToken);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"The call to the translation service returned HTTP status code {response.StatusCode}.");
+                }
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<TranslatorResponse[]>(responseBody);
+
+                return result?.FirstOrDefault();
+            }
+        }
+    }
+}
---
+++ MachineTranslation/TranslatorResponse.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MachineTranslation
+{
+    public class TranslatorResponse
+    {
+        [JsonProperty("detectedLanguage")]
+        public DetectedLanguage DetectedLanguage { get; set; }
+
+        [JsonProperty("translations")]
+        public IEnumerable<TranslatorResult> Translations { get; set; }
+    }
+
+    public class TranslatorResult
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("to")]
+        public string To { get; set; }
+    }
+
+    public class DetectedLanguage
+    {
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("score")]
+        public long Score { get; set; }
+    }
+}
